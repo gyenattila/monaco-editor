c19969240ab454454188dd68ba9c81815ad0e596
gyenattila <gyenattila@gmail.com>
2023-01-20 19:22:56 +0100

    new impl 2023.01.20

--- browser-esm-webpack/index.js
+++ browser-esm-webpack/index.js
@@ -24,7 +24,9 @@ const onReaderLoad = e => {
   selectedFunctionHeader.innerHTML = '';
 
   /** Parse the new file and create a JSON formatted content. */
-  const editorList = JSON.parse(e.target.result)
+  const editorList = JSON.parse(e.target.result);
+
+  console.log(editorList);
 
   const functionNamesSet = new Set();
 
@@ -42,20 +44,31 @@ const onReaderLoad = e => {
   for (const listElem of document.getElementsByTagName('li')) {
     listElem.addEventListener('click', e => {
       monacoContainerDiv.innerHTML = '';
-      selectedFunctionHeader.innerHTML = e.target.innerText;
+      selectedFunctionHeader.innerHTML = `Files mentioning function "${e.target.innerText}"`;
 
       const filteredElems = editorList.filter(
         listItem => listItem.functionName === e.target.innerText
       );
 
+      
+
       for (const elem of filteredElems) {
         const div = document.createElement('div');
+        const divTitle = document.createElement('h3');
+        const nrRows = elem.fileContent.split(/\r\n|\r|\n/).length;
+
+        divTitle.innerHTML = elem.fileName;
         div.id = elem.id;
-        div.style.width = '80%';
-        div.style.height = '300px';
+        div.style.width = '100%';
+        div.style.height = `${nrRows * 25}px`;
         div.style.border = '1px solid black';
         div.style.margin = '0 auto 20px auto';
+
+        monacoContainerDiv.appendChild(divTitle)
         monacoContainerDiv.appendChild(div)
+
+        monacoContainerDiv.style.width = '80%';
+        monacoContainerDiv.style.margin = '0 auto';
 
         createNewMEInstance(elem);
       }
@@ -74,14 +87,14 @@ const onReaderLoad = e => {
     const { missingNoExcepts } = elem;
 
     for (const missingNoExcept of missingNoExcepts) {
-      const  { message, lineNumber } = missingNoExcept;
+      const  { message, lineNumber, colNumber } = missingNoExcept;
       markers.push({
         message: message,
         severity: monaco.MarkerSeverity.Warning,
         startLineNumber: lineNumber,
         endLineNumber: lineNumber,
-        startColumn: model.getLineFirstNonWhitespaceColumn(lineNumber),
-        endColumn: model.getLineLength(lineNumber) + 1
+        startColumn:  model.getLineFirstNonWhitespaceColumn(lineNumber),
+        endColumn: colNumber //model.getLineLength(lineNumber) + 1
     });
     }
 
